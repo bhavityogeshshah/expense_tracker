a1d8d37045b0d2ffb0d349429a78476ab2275e6d
bhavityogeshshah <113150156+bhavityogeshshah@users.noreply.github.com>
2023-08-13 19:01:10 -0400

    Conditional Content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpenseFilter';
 import './Expenses.css';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -11,9 +12,11 @@ const Expenses = (props) => {
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
+
     const filteredExpense = props.items.filter((expense) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
+
     return (
         <div>
             <Card className='expenses'>
@@ -21,14 +24,7 @@ const Expenses = (props) => {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-                {filteredExpense.map((expense) => (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))}
+                <ExpensesList items={filteredExpense} />
             </Card>
         </div>
     );
